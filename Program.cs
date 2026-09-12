9a3db1487f5d9923e59e5fe1fd6696fbc969623e
Mori Keli <78599959+morikeli@users.noreply.github.com>
2026-09-12 14:06:28 +0300

    feat: initialize application with ASP.NET Core setup and service registrations

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterAPI.Db;
+using VideoGameCharacterAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterService>();
+builder.Services.AddDbContext<AppDbcontext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
